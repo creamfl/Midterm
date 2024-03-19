5972e49deee9692240fc8dbfc67c82c72fd82731
Niki Ramanchev <147237222+creamfl@users.noreply.github.com>
2024-03-19 17:12:22 +0100

    Update BulletinBoardClient.java

--- BulletinBoardClient.java
+++ BulletinBoardClient.java
@@ -1,20 +1,41 @@
-import java.net.*;
 import java.io.*;
-
+import java.net.Socket;
+import java.util.Scanner;
 
 public class BulletinBoardClient {
-    public static void main(String args[]) throws IOException {
-        String server = "localhost";
-        int port = 8080;
-        Socket client = new Socket(server, port);
+    private static final String SERVER_ADDRESS = "localhost";
+    private static final int PORT = 5000;
 
-        try (Socket socket = new Socket("localhost", 8080);
-             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-             BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))) {
-
+    public static void main(String[] args) {
+        try (
+            Socket socket = new Socket(SERVER_ADDRESS, PORT);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            Scanner scanner = new Scanner(System.in)
+        ) {
             String userInput;
-
+            while (true) {
+                System.out.println("Enter '1' to read messages, '2' to post a message, or 'QUIT' to exit:");
+                userInput = scanner.nextLine();
+                if ("1".equals(userInput)) {
+                    out.println("READ_MESSAGES");
+                    String response;
+                    while (!(response = in.readLine()).equals("EOF")) {
+                        System.out.println(response);
+                    }
+                } else if ("2".equals(userInput)) {
+                    System.out.println("Enter your message:");
+                    String message = scanner.nextLine();
+                    out.println("POST_MESSAGE:" + message);
+                    String response = in.readLine();
+                    System.out.println(response);
+                } else if ("QUIT".equals(userInput)) {
+                    out.println("QUIT");
+                    break;
+                } else {
+                    System.out.println("Invalid input. Please try again.");
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
